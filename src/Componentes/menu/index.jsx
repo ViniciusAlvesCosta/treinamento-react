141837d443e0617bb78a6fa0d2c21da85f7c512f
Vinicius Costa Sato <v.alvescosta26@gmail.com>
2023-01-17 20:12:16 -0300

    Banner

---
+++ src/Componentes/menu/index.jsx
@@ -0,0 +1,22 @@
+import styles from './Menu.module.css'
+
+import React from 'react'
+import MenuLink from '../menuLink';
+
+export default function Menu() {
+    
+
+  return (
+    <header>
+        <nav className={styles.navegacao}>
+            <MenuLink to="/">
+                Ínicio
+            </MenuLink>
+            <MenuLink to="/sobremim">
+                Sobre mim
+            </MenuLink>
+            
+        </nav>
+    </header>
+  )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Menu from "./Componentes/menu";
 import Inicio from "./paginas/inicio";
 import SobreMim from "./paginas/SobreMim";
 
 
-function App() {
+function AppRoutes() {
   return (
     <BrowserRouter>
+
+      <Menu />
+
       <Routes>
         <Route path="/" element={<Inicio />}/>
         <Route path="/sobremim" element={<SobreMim />} />
@@ -15,4 +19,4 @@ function App() {
   )
 }
 
-export default App;
+export default AppRoutes;
