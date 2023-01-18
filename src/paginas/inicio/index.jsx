055a62deeff05aa9a8073a580e7b1663f54f3839
Vinicius Costa Sato <v.alvescosta26@gmail.com>
2023-01-18 14:13:20 -0300

    Correcao

--- src/paginas/inicio/index.jsx
+++ src/paginas/inicio/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from './inicio.module.css'
 import posts from 'Json/posts.json'
-import PostsCard from 'Componentes/PostCard'
+import PostCard from 'Componentes/PostCard'
 
 export default function Inicio() {
   return (
       <ul className={styles.posts}>
           {posts.map((post) => (
             <li key={post.id}>
-                <PostsCard post={post}/>
+                <PostCard post={post}/>
             </li>
           ))}
       </ul>
